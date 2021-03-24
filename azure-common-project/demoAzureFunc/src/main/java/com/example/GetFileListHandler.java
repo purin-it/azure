1f60b66552ea1fa8b6676f58dd2699c87a36cbe9
purin-it <stanahas@amber.plala.or.jp>
2021-03-24 22:46:06 +0900

    azure-common-project

---
+++ azure-common-project/demoAzureFunc/src/main/java/com/example/GetFileListHandler.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.util.Optional;
+
+import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
+
+import com.example.model.GetFileListParam;
+import com.example.model.GetFileListResult;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+public class GetFileListHandler extends AzureSpringBootRequestHandler<GetFileListParam, GetFileListResult> {
+
+	/**
+	 * HTTP要求に応じて、DemoAzureFunctionクラスのfileUploadメソッドを呼び出し、その戻り値をボディに設定したレスポンスを返す.
+	 * 
+	 * @param request リクエストオブジェクト
+	 * @param context コンテキストオブジェクト
+	 * @return レスポンスオブジェクト
+	 */
+	@FunctionName("getFileList")
+	public HttpResponseMessage execute(
+			@HttpTrigger(name = "request", methods = HttpMethod.POST, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+			ExecutionContext context) {
+
+		// handleRequestメソッド内でDemoAzureFunctionクラスのfileUploadメソッドを呼び出し、
+		// その戻り値をボディに設定したレスポンスを、JSON形式で返す
+		return request.createResponseBuilder(HttpStatus.OK).body(handleRequest(new GetFileListParam(), context))
+				.header("Content-Type", "text/json").build();
+	}
+}
