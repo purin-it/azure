3508dd859cffc94cd3cf3aa86cf66f894debfed9
purin-it <stanahas@amber.plala.or.jp>
2023-11-18 14:10:28 +0900

    azure-functions-csv-to-db-ms932-comma

---
+++ azure-functions-csv-to-db-ms932-comma/demoAzureFunc/src/main/java/com/example/TimerTriggerTestHandler.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
+
+import com.example.model.TimerTriggerParam;
+import com.example.model.TimerTriggerResult;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
+
+public class TimerTriggerTestHandler extends FunctionInvoker<TimerTriggerParam, TimerTriggerResult>{
+
+	/**
+	 * TimerTriggerによって、DemoAzureFunctionクラスのtimerTriggerTestメソッドを呼び出す.
+	 * @param timerInfo TimerTriggerイベント情報
+	 * @param context コンテキストオブジェクト
+	 */
+	// 「schedule = "0 */2 * * * *"」で、2分毎にTimerTriggerイベントが発生するようになっている
+	@FunctionName("timerTriggerTest")
+	public void timerTriggerTest(@TimerTrigger(name = "timerTriggerTest", schedule = "0 */2 * * * *") String timerInfo,
+			ExecutionContext context) {
+		context.getLogger().info("TimerTriggerTestHandler timerTriggerTest triggered: " + timerInfo);
+		
+		TimerTriggerParam param = new TimerTriggerParam();
+		param.setTimerInfo(timerInfo);
+		
+		handleRequest(param, context);
+	}
+	
+}
