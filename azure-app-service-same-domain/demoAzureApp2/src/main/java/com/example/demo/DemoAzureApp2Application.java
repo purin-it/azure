1f539caf7c9d286961f17d8d04adcba1c460a706
purin-it <stanahas@amber.plala.or.jp>
2024-01-20 22:08:16 +0900

    azure-app-service-same-domain

---
+++ azure-app-service-same-domain/demoAzureApp2/src/main/java/com/example/demo/DemoAzureApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAzureApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAzureApp2Application.class, args);
+	}
+
+}
